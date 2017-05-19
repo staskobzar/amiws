1c09aae816bff0811eff9e8ce15586ba06519f2c
Stas Kobzar <stas@modulis.ca>
2017-05-18 23:22:39 -0400

    Calls history chart and dest map.

--- web_root/visual.js
+++ web_root/visual.js
@@ -2,12 +2,54 @@
  * Data visualization with charts
  */
 var Visual = (function(){
+
+  var mapdata = [
+      ['fo', 0], ['us', 0], ['jp', 0], ['in', 0], ['kr', 0], ['fr', 0],
+      ['cn', 0], ['sw', 0], ['sh', 0], ['ec', 0], ['au', 0], ['ph', 0],
+      ['es', 0], ['bu', 0], ['mv', 0], ['sp', 0], ['ve', 0], ['gb', 0],
+      ['gr', 0], ['dk', 0], ['gl', 0], ['pr', 0], ['um', 0], ['vi', 0],
+      ['ca', 0], ['tz', 0], ['cv', 0], ['dm', 0], ['sc', 0], ['nz', 0],
+      ['ye', 0], ['jm', 0], ['om', 0], ['vc', 0], ['bd', 0], ['sb', 0],
+      ['lc', 0], ['no', 0], ['cu', 0], ['kn', 0], ['bh', 0], ['fi', 0],
+      ['id', 0], ['mu', 0], ['se', 0], ['ru', 0], ['tt', 0], ['br', 0],
+      ['bs', 0], ['pw', 0], ['ir', 0], ['cl', 0], ['gw', 0], ['gd', 0],
+      ['ee', 0], ['ag', 0], ['tw', 0], ['fj', 0], ['bb', 0], ['it', 0],
+      ['mt', 0], ['pg', 0], ['de', 0], ['vu', 0], ['gq', 0], ['cy', 0],
+      ['km', 0], ['va', 0], ['sm', 0], ['az', 0], ['am', 0], ['sd', 0],
+      ['ly', 0], ['tj', 0], ['ls', 0], ['uz', 0], ['pt', 0], ['mx', 0],
+      ['ma', 0], ['ar', 0], ['sa', 0], ['nl', 0], ['ae', 0], ['ke', 0],
+      ['my', 0], ['ht', 0], ['do', 0], ['hr', 0], ['th', 0], ['cd', 0],
+      ['kw', 0], ['ie', 0], ['mm', 0], ['ug', 0], ['kz', 0], ['tr', 0],
+      ['er', 0], ['tl', 0], ['mr', 0], ['dz', 0], ['pe', 0], ['ao', 0],
+      ['mz', 0], ['cr', 0], ['pa', 0], ['sv', 0], ['kh', 0], ['gt', 0],
+      ['bz', 0], ['kp', 0], ['gy', 0], ['hn', 0], ['il', 0], ['ni', 0],
+      ['mw', 0], ['tm', 0], ['zm', 0], ['nc', 0], ['za', 0], ['lt', 0],
+      ['et', 0], ['gh', 0], ['si', 0], ['ba', 0], ['jo', 0], ['sy', 0],
+      ['mc', 0], ['al', 0], ['uy', 0], ['cnm', 0], ['mn', 0], ['rw', 0],
+      ['sx', 0], ['bo', 0], ['ga', 0], ['cm', 0], ['cg', 0], ['eh', 0],
+      ['me', 0], ['rs', 0], ['bj', 0], ['tg', 0], ['af', 0], ['ua', 0],
+      ['sk', 0], ['jk', 0], ['pk', 0], ['bg', 0], ['ro', 0], ['qa', 0],
+      ['li', 0], ['at', 0], ['sz', 0], ['hu', 0], ['ne', 0], ['lu', 0],
+      ['ad', 0], ['ci', 0], ['sl', 0], ['lr', 0], ['bn', 0], ['be', 0],
+      ['iq', 0], ['ge', 0], ['gm', 0], ['ch', 0], ['td', 0], ['ng', 0],
+      ['kv', 0], ['lb', 0], ['dj', 0], ['bi', 0], ['sr', 0], ['sn', 0],
+      ['gn', 0], ['zw', 0], ['pl', 0], ['mk', 0], ['py', 0], ['by', 0],
+      ['lv', 0], ['bf', 0], ['ss', 0], ['na', 0], ['la', 0], ['co', 0],
+      ['ml', 0], ['cz', 0], ['cf', 0], ['sg', 0], ['vn', 0], ['tn', 0],
+      ['mg', 0], ['eg', 0], ['so', 0], ['is', 0], ['lk', 0], ['np', 0],
+      ['kg', 0], ['md', 0], ['bt', 0], ['bw', 0]
+  ];
+
   var total_calls;
   var map;
+  var calls_history;
+  var indexes = new Array();
 
   function Visual(){
     total_calls = createTotalCalls();
     map = createMapChart();
+    calls_history = createCallsHistChart();
+    mapdata.forEach(function(el){indexes.push(el['hc-key']);});
   }
 
   /**
@@ -73,6 +115,14 @@ var Visual = (function(){
     Highcharts.charts[gaugeid].series[0].setData([servcalls, totalcalls - servcalls])
   }
 
+  Visual.prototype.updateMap = function(country) {
+    var idx = indexes.indexOf(country);
+    var val = 0;
+    if(idx < 0) return;
+    val = map.series[0].points[idx].value + 1;
+    map.series[0].points[idx].update({value: val});
+  }
+
   /**
    * Private functions
    */
@@ -119,18 +169,61 @@ var Visual = (function(){
     });
   }
 
+  function createCallsHistChart(){
+    return new Highcharts.Chart({
+      chart: {
+        zoomType: 'x',
+        events: {
+          load: function () {
+            var series = this.series[0];
+            setInterval(function () {
+              var x = (new Date()).getTime(),
+                  y = parseInt($("#ami-servers-list").attr('data-total-calls'));
+              series.addPoint([x, y], true, true);
+            }, 1000);
+          }
+        },
+        renderTo: 'callschart',
+        type: 'area',
+        height: 100
+      },
+      title: {
+        text: 'Last 10 min calls history',
+        style: { "color": "#333333", "fontSize": "11px" }
+      },
+      subtitle: { enabled: false },
+      legend: { enabled: false },
+      credits: { enabled: false },
+      xAxis: { visible: false, type: 'datetime' },
+      yAxis: { title: null },
+      plotOptions: {
+        area:{
+          lineWidth: 1,
+          marker: { radius: 1 },
+          states: { hover: { lineWidth: 1 } },
+        }
+      },
+      series: [{
+        name: 'Calls',
+        data: (function(){
+          var data = [], i = 0, time = (new Date()).getTime();
+          for (i = -600; i <= 0; i += 1) {
+            data.push([ (time + i * 1000), 0 ]);
+          }
+          return data;
+        }())
+      }]
+    });
+  }
+
   function createMapChart() {
     return new Highcharts.mapChart('callsmap',{
       chart: {
         map: 'custom/world',
       },
-
       title: null,
-
       subtitle: { enabled: false },
-
       legend: { enabled: false },
-
       credits: { enabled: false },
 
       mapNavigation: {
@@ -140,19 +233,13 @@ var Visual = (function(){
         }
       },
 
+      colorAxis: {
+        min: 0
+      },
+
       series: [{
-        name: 'Countries',
-        color: '#E0E0E0',
-        enableMouseTracking: false
-      }, {
-        type: 'mapbubble',
-        name: 'By country',
-        joinBy: ['iso-a2', 'code'],
-        minSize: 4,
-        maxSize: '12%',
-        tooltip: {
-          pointFormat: '{point.code}: {point.z} calls'
-        }
+        name: 'Calls destination by country',
+        data: mapdata
       }]
     });
   }
