591818dda02ee2fbb929945a3b5b0badc2c46e60
Stas Kobzar <stas@modulis.ca>
2017-05-19 23:24:25 -0400

    Fix build on redhat 6

--- config.h
+++ config.h
@@ -34,6 +34,9 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
 /* Name of package */
 #define PACKAGE "amiws"
 
@@ -48,9 +51,6 @@
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "amiws"
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "1.0.0"
